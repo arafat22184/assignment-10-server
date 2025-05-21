522967ef703e066f86d479750b94096b20b2be1e
arafat22184 <123alarafat@gmail.com>
2025-05-22 03:31:18 +0600

    added delete method

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ async function run() {
       res.send(result);
     });
 
+    // Delete User data
     app.delete("/userGroups/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
