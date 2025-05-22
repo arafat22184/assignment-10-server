1287fa90d91bd7f589e162fc6b3d130ed9eb7e6d
arafat22184 <123alarafat@gmail.com>
2025-05-22 19:17:50 +0600

    Deploy to vercel

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function run() {
       res.send(result);
     });
 
-    // Delete Group Datas
+    // Delete Group Data
     app.delete("/allGroups/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
